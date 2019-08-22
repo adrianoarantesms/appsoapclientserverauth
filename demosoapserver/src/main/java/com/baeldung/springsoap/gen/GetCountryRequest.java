02de8e009fa129597990eb820422694c83dfc5f3
Adriano <adrianoarantesms@gmail.com>
2019-08-22 09:25:05 -0400

    removido tarjet demo server

--- demosoapserver/src/main/java/com/baeldung/springsoap/gen/GetCountryRequest.java
+++ demosoapserver/src/main/java/com/baeldung/springsoap/gen/GetCountryRequest.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2019.08.21 às 04:49:11 PM PYT 
+// Gerado em: 2019.08.22 às 09:24:44 AM PYT 
 //
 
 
