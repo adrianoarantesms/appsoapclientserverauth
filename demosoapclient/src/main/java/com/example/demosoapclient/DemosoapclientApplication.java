dc9553f5b1986338353e56a31a6b32ee13c30541
Adriano <adrianoarantesms@gmail.com>
2019-08-22 09:19:46 -0400

    commit initial

---
+++ demosoapclient/src/main/java/com/example/demosoapclient/DemosoapclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosoapclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosoapclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosoapclientApplication.class, args);
+	}
+
+}
